b4bbdbdf9b04d9cb5e2ae515fba469483feffaa1
Asad Ullah <asad_622@hotmail.com>
2023-12-15 17:02:46 +0500

    -changes

--- Package.swift
+++ Package.swift
@@ -1,31 +1,34 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-// MainApp/Package.swift
-
-// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
-    name: "MainApp",
+    name: "MonoRepo",
     platforms: [
-        .iOS(.v14),
-        .macOS(.v11),
+        .iOS(.v14)
     ],
     products: [
-        .executable(name: "MainApp", targets: ["MainApp"]),
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "MonoRepo",
+            targets: ["MonoRepo"]),
     ],
     dependencies: [
         .package(path: "Package1"),
         .package(path: "Package2"),
     ],
     targets: [
-        .executableTarget(
-            name: "MainApp",
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "MonoRepo",
             dependencies: [
                 "Package1",
                 "Package2",
-            ]
-        )
+            ]),
+        .testTarget(
+            name: "MonoRepoTests",
+            dependencies: ["MonoRepo"]),
     ]
 )
