24db22ed6682745fc08e4826ff8b36e96b197b42
Asad Ullah <asad_622@hotmail.com>
2023-12-18 14:15:46 +0500

    - changes

--- Package1/Sources/Package1/Package1.swift
+++ Package1/Sources/Package1/Package1.swift
@@ -1,2 +1,15 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+import Package2
+
+public class Package1Class  {
+    
+    public var Package1Class = "Package1Class"
+    
+    public init(Package1Class: String = "Package1Class") {
+        self.Package1Class = Package1Class
+        print(Package1Class)
+    }
+}
--- Package2/Sources/Package2/Package2.swift
+++ Package2/Sources/Package2/Package2.swift
@@ -1,2 +1,14 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Package1
+
+public class Package2Class  {
+    
+    public var Package2Class = "Package2Class"
+    
+    public init(Package1Class: String = "Package2Class") {
+        self.Package2Class = Package1Class
+        print(Package1Class)
+    }
+}
