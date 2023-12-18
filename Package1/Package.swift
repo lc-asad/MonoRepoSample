505cd518c774a303b48fba5e3f9b1af87f85408b
Asad Ullah <asad_622@hotmail.com>
2023-12-18 18:39:50 +0500

    - changes

--- Package1/Package.swift
+++ Package1/Package.swift
@@ -7,19 +7,17 @@ let package = Package(
     name: "Package1",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Package1",
-            targets: ["Package1"]),
+        .library(name: "Package1", targets: ["Package1"]),
+        .library(name: "Package2", targets: ["Package2"]),
     ],
     dependencies: [
-        .package(path: "../Package2")
+//        .package(path: "../Package2")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Package1",dependencies: [], path: "./Package2/Sources/"
-        ),
+            name: "Package2",dependencies: [], path: "./Package2/Sources/"),
         .testTarget(
             name: "Package1Tests",
             dependencies: ["Package1"]),
