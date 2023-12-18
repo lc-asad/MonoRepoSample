e8fcf5c450e80c642e9c7dbfb3a76a59eaa2f3db
Asad Ullah <asad_622@hotmail.com>
2023-12-18 16:45:05 +0500

    - hcnages

--- Package1/Package.swift
+++ Package1/Package.swift
@@ -11,11 +11,16 @@ let package = Package(
             name: "Package1",
             targets: ["Package1"]),
     ],
+    dependencies: [
+        .package(path: "../Package2")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Package1"),
+            name: "Package1",
+            dependencies: ["Package2"]
+        ),
         .testTarget(
             name: "Package1Tests",
             dependencies: ["Package1"]),
--- Package1/Sources/Package1/Package1.swift
+++ Package1/Sources/Package1/Package1.swift
@@ -2,14 +2,15 @@
 // https://docs.swift.org/swift-book
 
 import Foundation
-//import Package2
+import Package2
 //
-//public class Package1Class  {
-//    
-//    public var Package1Class = "Package1Class"
-//    
-//    public init(Package1Class: String = "Package1Class") {
-//        self.Package1Class = Package1Class
-//        print(Package1Class)
-//    }
-//}
+public class Package1Class  {
+    
+    public var Package1Class = "Package1Class"
+    
+    public init(Package1Class: String = "Package1Class") {
+        self.Package1Class = Package1Class
+        let package2 = Package2Class()
+        print(Package1Class + package2.Package2Class)
+    }
+}
