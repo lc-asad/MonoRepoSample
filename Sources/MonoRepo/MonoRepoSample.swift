27a988a3e4f9026b254ec0ef18152b7c78d48095
Asad Ullah <asad_622@hotmail.com>
2023-12-18 13:27:02 +0500

    - changes

---
+++ Sources/MonoRepo/MonoRepoSample.swift
@@ -0,0 +1,14 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+import Foundation
+
+public class MonoRepoSample {
+    
+    public var sample = "Sample here"
+    public init() {
+        print(sample)
+    }
+    
+}
