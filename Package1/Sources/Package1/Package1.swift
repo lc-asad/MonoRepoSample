2719dd08dfd440cb98df8d57594449538e72fa7a
Asad Ullah <asad_622@hotmail.com>
2023-12-18 14:23:46 +0500

    - changes

--- Package1/Sources/Package1/Package1.swift
+++ Package1/Sources/Package1/Package1.swift
@@ -2,14 +2,14 @@
 // https://docs.swift.org/swift-book
 
 import Foundation
-import Package2
-
-public class Package1Class  {
-    
-    public var Package1Class = "Package1Class"
-    
-    public init(Package1Class: String = "Package1Class") {
-        self.Package1Class = Package1Class
-        print(Package1Class)
-    }
-}
+//import Package2
+//
+//public class Package1Class  {
+//    
+//    public var Package1Class = "Package1Class"
+//    
+//    public init(Package1Class: String = "Package1Class") {
+//        self.Package1Class = Package1Class
+//        print(Package1Class)
+//    }
+//}
